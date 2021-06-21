451b33612624636d87f6a7b0cdd1d3a9de6d15d3
Andre Hollis <andrehollis40@gmail.com>
2021-06-22 00:15:18 +1000

    Added hello world

--- src/Clock.js
+++ src/Clock.js
@@ -23,7 +23,7 @@ class Clock extends React.Component {
     render(){
         return(
             <div>
-                <h1></h1>
+                <h1>Hello world!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
