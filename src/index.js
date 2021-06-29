3cce9e4479d95be5eaf5f92db42e0eec813ecef7
Andre Hollis <andrehollis40@gmail.com>
2021-06-29 17:30:05 +1000

    Updated stuff

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Clock from './pages/clockPage/Clock';
-import Count from './pages/countPage/Count';
-import TopNav from './components/TopNav';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <TopNav />
-    <Clock />
-    <Count />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render( <App />,document.getElementById('root'));
