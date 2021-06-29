652d450d70b592a6ba706bc679e6f87082eecb36
Andre Hollis <andrehollis40@gmail.com>
2021-06-29 16:49:48 +1000

    Updated stuff

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import './index.css';
 import App from './App';
 import Clock from './pages/clockPage/Clock';
 import Count from './pages/countPage/Count';
+import TopNav from './components/TopNav';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TopNav />
     <Clock />
     <Count />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
